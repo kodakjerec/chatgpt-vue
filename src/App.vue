a73fd75c1ecdc8247fbc7d7d757caca2a26a1440
kodak <kodakjerec@gmail.com>
2023-04-21 10:41:20 +0800

    加上刪除對話

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@
         <li class="py-2 border-t border-gray-700 flex items-center" v-for="item in logList">
           <div :class="{ 'text-green': selectLog === item }" @click="clickLogName(item)">{{ item }}</div>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="-12 0 36 24" stroke-width="1.5"
-            stroke="currentColor" class="w-6 h-6 trashCan">
+            stroke="currentColor" class="w-6 h-6 trashCan" v-show="logList.length > 1" @click="delChatLog(item)">
             <path stroke-linecap="round" stroke-linejoin="round"
               d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
           </svg>
@@ -49,7 +49,7 @@ export default {
   },
   methods: {
     /**
-     * 取得所有的對話紀錄
+     * 取得所有的 對話紀錄清單
      */
     getLogList() {
       let logList = localStorage.getItem('logList');
@@ -59,6 +59,9 @@ export default {
         this.logList = ["chatGPT"];
       }
     },
+    /**
+     * 儲存 對話紀錄清單
+     */
     setLogList() {
       localStorage.setItem('logList', JSON.stringify(this.logList));
     },
@@ -88,12 +91,30 @@ export default {
       this.clickLogName(generatedString);
       this.setLogList();
     },
-    // 按下chatlog
+    /**
+     * 刪除一個對話
+     */
+    delChatLog(logName) {
+      if(this.logList.length<2) return;
+
+      localStorage.removeItem(logName);
+      let findIndex = this.logList.findIndex(element => element === logName);
+      if (findIndex>-1) {
+        this.logList.splice(findIndex, 1);
+        this.setLogList();
+      }
+      this.clickLogName(this.logList[0]);
+    },
+    /**
+     * 按下按鈕 chatlog
+     */ 
     clickLogName(logName) {
       this.selectLog=logName;
       this.gotoChat();
     },
-    // 跳頁
+    /**
+     * 跳頁
+     */ 
     gotoChat() {
       this.$router.push({ name: 'chat', params: { sendLogName: this.selectLog } })
     }
