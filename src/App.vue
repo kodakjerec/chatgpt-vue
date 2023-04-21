cbc80706543c9153d2a32261ffe6074b8125b65a
TAIPEI\mPOS109 <kodakjerec@gmail.com>
2023-04-21 18:03:47 +0800

    調整click範圍

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,14 @@
     </div>
     <div class="bg-gray-800 text-white px-4 sidebar" :class="{ active: sidebarActive }">
       <ul class="mt-8 flex flex-col justify-between cursor-pointer">
-        <li class="py-2 border-t border-gray-700 flex items-center">
-          <span @click="toggleSidebar">Hide Menu</span>
+        <li class="py-2 border-t border-gray-700 flex items-center" @click="toggleSidebar">
+          <span>Hide Menu</span>
         </li>
-        <li class="py-2 border-t border-gray-700 flex items-center hover:bg-slate-700 hover:rounded">
-          <span @click="goHome()">Home</span>
+        <li class="py-2 border-t border-gray-700 flex items-center hover:bg-slate-700 hover:rounded" @click="goHome()">
+          <span>Home</span>
         </li>
-        <li class="py-2 border-t border-gray-700 flex items-center hover:bg-slate-700 hover:rounded">
-          <div class="flex items-center" @click="newChatLog()">
+        <li class="py-2 border-t border-gray-700 flex items-center hover:bg-slate-700 hover:rounded" @click="newChatLog()">
+          <div class="flex items-center">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 36 24" stroke-width="1.5"
               stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
@@ -21,10 +21,10 @@
             New Chat
           </div>
         </li>
-        <li class="py-2 border-t border-gray-700 flex items-center hover:bg-slate-700 hover:rounded"
+        <li class="py-2 border-t border-gray-700 flex items-center hover:bg-slate-700 hover:rounded" @click="clickLogName(item)"
           v-for="(item, index) in logList" :key="index">
           <div :class="{ 'text-yellow-300': selectLog === item }" v-if="item !== editing" @dblclick="editLogName(item)"
-            @click="clickLogName(item)">{{ item }}</div>
+            >{{ item }}</div>
           <input type="text" class="text-black bg-slate-400" v-else @blur="updateLogName(index)" v-model="newLogName"
             ref="editingLogName">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="-12 0 36 24" stroke-width="1.5"
@@ -34,8 +34,8 @@
               d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
           </svg>
         </li>
-        <li class="mt-auto py-2 border-t border-b border-gray-700 hover:bg-slate-700 hover:rounded">
-          <router-link to="/settings">Settings</router-link>
+        <li class="mt-auto py-2 border-t border-b border-gray-700 hover:bg-slate-700 hover:rounded" @click="goSettings()">
+          <span>Settings</span>
         </li>
       </ul>
     </div>
@@ -164,6 +164,9 @@ export default {
     },
     goHome() {
       this.$router.push({ name: 'home' });
+    },
+    goSettings() {
+      this.$router.push({ name: 'settings' });
     },
     toggleSidebar() {
       this.sidebarActive = !this.sidebarActive;
