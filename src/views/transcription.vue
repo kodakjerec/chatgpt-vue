1c57f9f0b51935ebc464d44537acc0ddba002555
kodak <kodakjerec@gmail.com>
2023-05-04 17:35:04 +0800

    fix ts

--- src/views/transcription.vue
+++ src/views/transcription.vue
@@ -6,32 +6,26 @@
             <div class="w-full">
                 <div class="flex flex-col">
                     <input type="file" ref="fileInput" @change="handleFileSelect" :disabled="isLoading">
-                    <div
-                    class="border border-dashed border-blue-500 p-10 text-center"
-                    @dragover.prevent
-                    @drop="handleDrop"
-                    @dragenter="isDragging = true"
-                    @dragleave="isDragging = false"
-                    :class="{ 'dragging': isDragging }"
-                    >
-                    <p>Drag and drop files here</p>
-                    <p>The audio file to translate, in one of these formats: mp3, mp4, mpeg, mpga, m4a, wav, or webm.</p>
+                    <div class="border border-dashed border-blue-500 p-10 text-center" @dragover.prevent @drop="handleDrop"
+                        @dragenter="isDragging = true" @dragleave="isDragging = false" :class="{ 'dragging': isDragging }">
+                        <p>Drag and drop files here</p>
+                        <p>The audio file to translate, in one of these formats: mp3, mp4, mpeg, mpga, m4a, wav, or webm.
+                        </p>
                     </div>
                 </div>
                 <label for="prompt" class="text-gray-700 mb2 flex items-center">
                     <span class="w-1/4">prompt</span>
-                    <input v-model.number.trim.lazy="prompt"
-                            class="input"
-                            id= "prompt"
-                            name= "prompt"
-                            placeholder="An optional text to guide the model's style or continue a previous audio segment. The prompt should be in English."
-                            />
+                    <input v-model.number.trim.lazy="prompt" class="input" id="prompt" name="prompt"
+                        placeholder="An optional text to guide the model's style or continue a previous audio segment. The prompt should be in English." />
                 </label>
             </div>
             <!-- Text -->
             <div class="w-full flex flex-col">
-                <p class="flex">transcription Result<Loding class="mt-1" v-if="isLoading" /></p>
-                <textarea :disabled="isLoading" v-model="result" class="input w-full text-justify" rows="18" placeholder="transcription Result" ></textarea>
+                <p class="flex">transcription Result
+                    <Loding class="mt-1" v-if="isLoading" />
+                </p>
+                <textarea :disabled="isLoading" v-model="result" class="input w-full text-justify" rows="18"
+                    placeholder="transcription Result"></textarea>
             </div>
         </div>
     </div>
@@ -65,11 +59,12 @@ export default {
     },
     methods: {
         handleFileSelect(event: Event) {
+            if (!event.target) return;
             this.isLoading = true;
             this.result = "";
-            const file = (event.target as HTMLInputElement).files[0];
+            const file = (event.target as HTMLInputElement).files ?? [0];
             this.uploadFile(file);
-            event.target.value = "";
+            (event.target as HTMLInputElement).value = "";
         },
         handleDrop(event: DragEvent) {
             event.preventDefault();
@@ -78,13 +73,12 @@ export default {
             this.isDragging = false;
             this.result = "";
 
-            const file = event.dataTransfer?.files[0];
+            const file = (event.dataTransfer as DataTransfer).files[0];
             if (file) {
                 this.uploadFile(file);
             }
-            event.dataTransfer.value = "";
         },
-        async uploadFile(file:File) {
+        async uploadFile(file: File) {
             try {
                 const { body, status } = await audiotranscriptions(file, this.prompt);
                 if (body) {
@@ -103,28 +97,28 @@ export default {
          * @param status response回傳狀態
          */
         async readStream(
-        reader: ReadableStreamDefaultReader<Uint8Array>,
-        status: number
+            reader: ReadableStreamDefaultReader<Uint8Array>,
+            status: number
         ) {
-        let partialLine = "";
+            let partialLine = "";
 
-        while (true) {
-            // eslint-disable-next-line no-await-in-loop
-            const { done, value } = await reader.read();
-            if (done) break;
+            while (true) {
+                // eslint-disable-next-line no-await-in-loop
+                const { done, value } = await reader.read();
+                if (done) break;
 
-            const decodedText = this.decoder.decode(value, { stream: true });
-            if (status !== 200) {
-                const json = JSON.parse(decodedText); // start with "data: "
-                const content = json.error.message ?? decodedText;
-                this.appendLastMessageContent(content);
-                return;
+                const decodedText = this.decoder.decode(value, { stream: true });
+                if (status !== 200) {
+                    const json = JSON.parse(decodedText); // start with "data: "
+                    const content = json.error.message ?? decodedText;
+                    this.appendLastMessageContent(content);
+                    return;
+                }
+
+                // 回傳URL
+                let response = JSON.parse(decodedText);
+                this.result = response.text;
             }
-
-            // 回傳URL
-            let response = JSON.parse(decodedText);
-            this.result = response.text;
-        }
         },
     }
 
@@ -133,6 +127,6 @@ export default {
 
 <style scoped>
 .dragging {
-  background-color: #eee;
+    background-color: #eee;
 }
 </style>
