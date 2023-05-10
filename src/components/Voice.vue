ec6df2687d0c9465ade8a7157f2a2606350842e6
kodak <kodakjerec@gmail.com>
2023-05-10 15:28:00 +0800

    fix speech

--- src/components/Voice.vue
+++ src/components/Voice.vue
@@ -1,11 +1,11 @@
 <template>
     <voice-one theme="outline" size="24" fill="#333" class="hover:cursor-pointer" @click="speak()" v-if="!isSpeaking" />
-    <voice-one theme="multi-color" size="24" :fill="['#2F88FF' ,'#FFF' ,'#2F88FF' ,'#43CCF8']" v-else />
+    <voice-one theme="multi-color" size="24" :fill="['#2F88FF', '#FFF', '#2F88FF', '#43CCF8']" v-else />
 </template>
 <script lang="ts">
 import { VoiceOne } from "@icon-park/vue-next";
 
-export default  {
+export default {
     name: 'voice',
     components: {
         VoiceOne
@@ -18,9 +18,10 @@ export default  {
     },
     data() {
         return {
-            volume: 1, // sound
-            rate: .8, // speed
-            pauseLength: 100, //
+            volume: 1, // sound, 0~1, default:1
+            rate: 1, // speed, 0.1~10, default:1
+            pitch: 2, // pitch, 0~2, default:1
+            voice: 'Google 國語（臺灣）', // voice,
             lang: 'zh-TW', // language example:"en-US"、 "fr-FR", "es-ES","ja-JP"
             isSpeaking: false,
         }
@@ -32,13 +33,15 @@ export default  {
             let msg = new SpeechSynthesisUtterance();
             const synth = window.speechSynthesis;
             msg.text = this.content;
-            msg.volume = this.volume ;
-            msg.rate = this.rate ;
+            msg.volume = this.volume;
+            msg.rate = this.rate;
+            msg.pitch = this.pitch;
+            // msg.voice = this.voice;
             msg.lang = 'zh-TW';
-            msg.onstart = ()=> {
+            msg.onstart = () => {
                 this.isSpeaking = true;
             };
-            msg.onend = ()=> {
+            msg.onend = () => {
                 this.isSpeaking = false;
             };
 
